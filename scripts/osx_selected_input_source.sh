ca408eff242646f8319f459864b0e32489c655f1
An Nguyen <an.nguyenwasd@gmail.com>
2020-09-09 10:52:39 +0700

    [master] update icon

--- scripts/osx_selected_input_source.sh
+++ scripts/osx_selected_input_source.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-vn_icon=""
-us_icon=" "
+# vn_icon=""
+# us_icon=" "
+vn_icon="vi"
+us_icon="en"
 
 main() {
   local _input=$(echo $(defaults read com.apple.HIToolbox AppleSelectedInputSources  | xargs) | tr "[:upper:]" "[:lower:]")
